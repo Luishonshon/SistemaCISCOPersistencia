d19c1388060df34c8f6acf3df0a8660bb13e9947
luishonshon <luis.angel.castro.lara52@gmail.com>
2025-06-25 01:05:35 -0700

    Se agrega la dependencia de dominio

---
+++ src/main/java/com/mycompany/sistemaciscopersistencia/SistemaCISCOPersistencia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemaciscopersistencia;
+
+/**
+ *
+ * @author luishonshon
+ */
+public class SistemaCISCOPersistencia {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

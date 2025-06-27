6c8c6463f7cf1ae11e79cdfb8e64667da7950fad
sansanty03 <138645645+sansanty03@users.noreply.github.com>
2025-06-27 08:17:30 -0700

    Update CentroDAO.java

--- src/main/java/DAO/CentroDAO.java
+++ src/main/java/DAO/CentroDAO.java
@@ -13,6 +13,13 @@ import Dominio.Computadora;
 import Dominio.Regla;
 import Interfaces.ICentroDAO;
 import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 /**
  *
@@ -22,27 +29,78 @@ public class CentroDAO implements ICentroDAO{
 
     @Override
     public Centro agregarCentro(AgregarCentroDTO nuevoCentro) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("ConexionJPA");
+        EntityManager entityManager = fabrica.createEntityManager();
+        Centro centro = new Centro();
+        centro.setNombre(nuevoCentro.getNombre());
+        centro.setContraseña(nuevoCentro.getContraseña());
+        centro.setFechaFin(nuevoCentro.getFechaFin());
+        centro.setFechaInicio(nuevoCentro.getFechaInicio());
+        entityManager.getTransaction().begin();
+        entityManager.persist(centro);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        fabrica.close();
+        return centro;
+        
     }
 
     @Override
     public List<Centro> buscarCentros() {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("ConexionJPA");
+        EntityManager entityManager = fabrica.createEntityManager();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Centro> criteriaQuery = criteriaBuilder.createQuery(Centro.class);
+        List<Centro> centros = entityManager.createQuery(criteriaQuery).getResultList();
+        entityManager.close();
+        fabrica.close();
+        return centros;
     }
 
     @Override
     public Centro BuscarCentroID(Long Id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("ConexionJPA");
+        EntityManager entityManager = fabrica.createEntityManager();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Centro> criteriaQuery = criteriaBuilder.createQuery(Centro.class);
+        Root<Centro> root = criteriaQuery.from(Centro.class);
+        criteriaQuery.select(root)
+                     .where(criteriaBuilder.equal(root.get("id"), Id));
+        TypedQuery<Centro> typedQuery = entityManager.createQuery(criteriaQuery);
+        Centro centro = typedQuery.getSingleResult();
+        entityManager.close();
+        fabrica.close();
+        return centro;
     }
 
     @Override
     public Regla agregarRegla(AgregarReglaDTO nuevaRegla) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("ConexionJPA");
+        EntityManager entityManager = fabrica.createEntityManager();
+        Regla regla = new Regla();
+        regla.setDescripcion(nuevaRegla.getDescripcion());
+        entityManager.getTransaction().begin();
+        entityManager.persist(regla);
+        entityManager.getTransaction().commit();
+        entityManager.close();
+        fabrica.close();
+        return regla;
     }
 
     @Override
     public Regla BuscarReglaId(Long Id) {
-        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+        EntityManagerFactory fabrica = Persistence.createEntityManagerFactory("ConexionJPA");
+        EntityManager entityManager = fabrica.createEntityManager();
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Regla> criteriaQuery = criteriaBuilder.createQuery(Regla.class);
+        Root<Regla> root = criteriaQuery.from(Regla.class);
+        criteriaQuery.select(root)
+                     .where(criteriaBuilder.equal(root.get("id"), Id));
+        TypedQuery<Regla> typedQuery = entityManager.createQuery(criteriaQuery);
+        Regla regla = typedQuery.getSingleResult();
+        entityManager.close();
+        fabrica.close();
+        return regla;
     }
 
     @Override
